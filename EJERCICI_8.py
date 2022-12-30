5bb497e353a17a0aa3a7b0f084ff5ddb826db78e
Luis Fernando <luis.rivera.27@unsch.edu.pe>
2022-12-30 12:57:45 -0500

    EJERCICIO8:

--- EJERCICI_8.py
+++ EJERCICI_8.py
@@ -1,6 +1,12 @@
-def potencia(n):
-    if n % 2 == 0:
-        print("Es un número par")
-        A = 2*(2*potencia(n)/2)
-        print(A)
-potencia(4)
+def potencia(b,n):
+    if n <= 0:
+        respuesta = 1
+    elif n % 2 == 0:
+        pot = potencia(b, n/2)
+        respuesta = pot * pot
+    else:
+        pot = potencia(b, (n-1)/2)
+        respuesta = pot * pot * b
+    print(respuesta)
+    return respuesta
+potencia(3,3)
